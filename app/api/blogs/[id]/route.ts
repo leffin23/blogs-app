e505597f8c4d1d78309f1b67e5352b9988d374d9
leffin23 <mini.dasha.12@gmail.com>
2024-10-29 11:41:13 +0200

    Enabled deleting blogs

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { auth } from "@/auth";
 
 const prisma = new PrismaClient();
 
@@ -17,4 +18,43 @@ export async function GET(request:Request, {params}: {params:{id: string}}) {
         },
     })
     return NextResponse.json(blogInfo)
+}
+
+
+export async function DELETE(request:Request, {params}: {params:{id: string}}) {
+
+    const { id } = await params;
+    if (!id) {
+        return NextResponse.json({ error: 'Blog ID is required' }, { status: 400 });
+    }
+
+    const session = await auth(); 
+    if (!session || !session.user) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    try {
+ 
+        const blog = await prisma.blog.findUnique({
+            where: { id },
+            select: { userId: true }, 
+        });
+
+    
+        if (!blog) {
+            return NextResponse.json({ error: 'Blog not found' }, { status: 404 });
+        }
+
+        
+        if (blog.userId !== session.user.id) {
+            return NextResponse.json({ error: 'Unauthorized to delete this blog' }, { status: 403 });
+        }
+
+        await prisma.blog.delete({ where: { id } });
+        return NextResponse.json({ message: 'Blog deleted successfully' }, { status: 200 });
+
+    } catch (error) {
+        console.error('Error deleting blog:', error);
+        return NextResponse.json({ error: 'Could not delete blog' }, { status: 500 });
+    }
 }
---
+++ components/Blogs/ManageBlogBtns.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { deleteBlogPost } from '@/utils/api'
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import styles from "./Blogs.module.css"
+import { redirect } from 'next/navigation'
+
+interface ButtonsProps{
+    blogId: string
+}
+const ManageBlogBtns = ({blogId}: ButtonsProps) => {
+
+    const [loading, setLoading] = useState(false);
+    const handleDelete = (e) => {
+        e.preventDefault()
+        if(confirm("Are you sure you want to delete a blog?")){
+            deleteBlogPost(blogId)
+            .then((res) => {
+                if(res === '200'){
+                    redirect('/blogs/my-blogs')
+                }
+            })
+            setLoading(true)
+        }
+    }
+  return (
+    <div className={styles.btns}>
+       <button onClick={(e) => handleDelete(e)} disabled={loading}> {loading ? "Deleting..." : "Delete blog"}  <FontAwesomeIcon icon={faTrashCan}  /></button>
+       <button>Update blog <FontAwesomeIcon icon={faPenToSquare} /></button>
+    </div>
+  )
+}
+
+export default ManageBlogBtns
