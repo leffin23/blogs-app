19832c42ffe04fb11809c70e078f438b06db2470
leffin23 <mini.dasha.12@gmail.com>
2024-11-04 21:50:50 +0200

    Created API for liking/unliking

---
+++ app/api/blogs/likes/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { auth } from "@/auth";
+
+const prisma = new PrismaClient();
+
+
+export async function POST(request:Request) {
+
+    const requestBody = await request.json()
+    const {id} = requestBody
+
+    const session = await auth()
+
+    if(!session || !session?.user || !session.user.id){ 
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const existingLike = await prisma.like.findFirst({
+        where: {
+            userId: session.user.id, 
+            blogId: id
+        }
+    });
+    if (existingLike) {
+       
+        await prisma.like.delete({
+            where: {
+                id: existingLike.id, 
+            },
+        });
+        return NextResponse.json({ message: 'You have unliked this blog post.' }, { status: 200 });
+    } else{
+
+        const newLike = await prisma.like.create({
+            data:{
+                userId: session.user.id,
+                blogId: id,
+            }
+        })
+        return NextResponse.json({ message: 'You have liked this blog post.', like: newLike }, { status: 201 }); 
+    }
+
+}
