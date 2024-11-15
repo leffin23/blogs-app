c2cd37bce06c5d1d757fc27924a1e697c9450e97
leffin23 <mini.dasha.12@gmail.com>
2024-11-15 22:14:34 +0200

    Changed callback url after signing in

--- components/Navbar/AuthButtons.tsx
+++ components/Navbar/AuthButtons.tsx
@@ -10,7 +10,7 @@ const AuthButtons = ({ isSignedIn }: AuthButtonsProps) => {
   return isSignedIn ? (
     <div onClick={() => signOut()}>Sign out</div>
   ) : (
-    <div onClick={() => signIn("google")}>Login</div>
+    <div onClick={() => signIn("google", { callbackUrl: "/blogs/personal" })}>Login</div>
   );
 };
 
